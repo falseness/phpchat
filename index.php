27cd6db6bab8cf36506ca48db5f55b3ef4de09f5
falseness <40433365+falseness@users.noreply.github.com>
2018-12-10 14:56:29 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Соцсеть нового поколения!</title>
+    <style>
+        .chat
+        {
+            margin-top:5px;
+            width:500px;
+            border: 2px dashed green;
+            padding:5px;
+        }
+        .message
+        {
+            background: #e6e6e6;
+            padding:2px;
+            margin-bottom:5px;
+        }
+    </style>
+</head>
+<body>
+    <form method = "POST">
+        Имя<br><input type = "text" name = "username"><br>
+        Текст сообщения<br><input type = "text" name = "text"><br>
+        <br>
+        <input type = "submit" value = "Отправить">
+    </form>
+    <?php
+        $text = $_POST['text'];
+        $username = $_POST['username'];
+        
+        if (!empty($text) && !empty($username))
+        {
+            $file = fopen('history.txt', 'a+');
+            $t = file_get_contents('history.txt');
+            if (!empty($t))
+                fwrite($file, "\r\n");
+            fwrite($file, $username.' ('.(date('d').'.'.date('j').'.'.date('y').' '.date('H').':'.date('i')).'): '.$text);
+            fclose($file);
+        }
+    ?>
+    <div class = "chat">
+        <?php
+            $f = fopen('history.txt', 'a+');
+            $file_text = file_get_contents('history.txt');
+            $file_text = trim($file_text, "\n");
+            $file_text = str_replace("\n", '</div><div class = "message">', $file_text);
+            echo '<div class = "message">'.$file_text.'</div>';
+        ?>
+    </div>
+</body>
+</html>
